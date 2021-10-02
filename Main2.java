cd861f3985dc9faa3542f08207ddf5bf2f5afbbf
irmaazizah38 <86936133+irmaazizah38@users.noreply.github.com>
2021-10-03 00:46:28 +0700

    Create Main2.java
    Calculating salaried employee and hourly employee's salary per week.

---
+++ Main2.java
@@ -0,0 +1,27 @@
+
+/**
+ *
+ * @author fresk
+ */
+public class Main2 {
+    public static void main(String[] args) {
+    
+   
+    SalariedEmployee se = new SalariedEmployee("irma azizah",  "05.855.081.4-412.000");    
+    HourlyEmployee he = new HourlyEmployee("jane ruby","05.856.132.3-412.000" ); 
+    
+    se.setSalary(37440);
+    he.setHourlyPayment(10);
+    he.setWorkedHours(6);
+    
+    Employee[] employee = new Employee[2];
+    employee[0] =  se;
+    employee[1] =  he;
+
+        for (Employee employee1 : employee) {
+            System.out.println( employee1.payment() );
+            
+        }
+    
+    }
+}
